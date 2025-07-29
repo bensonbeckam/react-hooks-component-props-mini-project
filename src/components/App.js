87ddad6f87273ed4bc544015c6d8bf331c8429be
Beckham <bensonbeckham98@gmail.com>
2025-07-29 16:53:41 +0300

    w

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+ import React from "react";
 import blogData from "../data/blog";
 
 console.log(blogData);
@@ -6,8 +6,12 @@ console.log(blogData);
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <main>
+      <aside>
+     <header>You're on your own from here! Follow the deliverables; test things out in
+      the browser as you write your code; and good luck!</header> 
+      </aside>
+      </main>
     </div>
   );
 }
